460818b217f028af2631660e58fd09174c35bcfe
Hbouazzaoui <bouazzaouiihamouda@gmail.com>
2025-04-26 20:19:00 +0100

    Add Style css in navbar and add category.service.ts

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { User } from '../../models/user.model';
+import { CategoryService } from '../../services/category.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  currentUser$ = this.authService.currentUser$;
+  categories: Observable<string[]>; 
 
+  constructor(
+    private authService: AuthService,
+    private categoryService: CategoryService
+  ) {
+    this.categories = this.categoryService.getAllCategories();
+  }
+
+  ngOnInit(): void {}
+
+  logout(event: Event): void {
+    event.preventDefault();
+    this.authService.logout();
+  }
 }
